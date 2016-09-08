900fb6f05f13244bc83341e7dc7ef225eb4ddc59
Time-Jang <jww0227@gmail.com>
2016-09-08 14:57:19 +0900

    HI

---
+++ lab1/ex1.c
@@ -0,0 +1,10 @@
+#inlcude<stdio.h>
+int main(void){
+	int press = 0;
+	scanf("%d",&press);
+	if(press==1)
+  	  printf("jww0227@hanyang.ac.kr.");
+	if(press==2)
+  	  printf("01051996349");
+	return 0;
+}
